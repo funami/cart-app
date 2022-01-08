f5fc8fc524a689d0eaeae9c7b29941477172c192
Takao Funami <takao.funami@gmail.com>
2022-01-08 21:28:21 +0900

    store_jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_jwt, if: :devise_controller?
 
   protected
 
@@ -7,5 +8,15 @@ class ApplicationController < ActionController::Base
     added_attrs = %i[username email password password_confirmation remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def store_jwt
+    if current_user
+      cookies[:firebase_custom_token] = {
+        value: current_user.firebase_custom_token, path: "/"
+      }
+    else
+      cookies.delete :firebase_custom_token
+    end
   end
 end
